24e33de6b1459ce21fb9ed0529dd2bf1d3799b05
Joseph Stalnaker <josephlstalnaker@gmail.com>
2019-09-21 18:51:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board
-   puts " #{board[0]} | #{board[1]} | #{board[2] #{board[0] | #{board[1]} |  #{board[2]} "
+   puts " #{board[0]} | #{board[1]} | #{board[2]} #{board[0]} | #{board[1]} |  #{board[2]} "
    
+   puts "-----------"
    
